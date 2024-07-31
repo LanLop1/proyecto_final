1e47c9ac260283c5ef66d58de8ded55fb1fc5075
LanLop1 <lanchazolopezs@gmail.com>
2024-07-31 17:52:41 +0200

    Cambios

--- django-starter/a_core/asgi.py
+++ django-starter/a_core/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Proyecto_Final project.
+ASGI config for a_core project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Proyecto_Final.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'a_core.settings')
 
 application = get_asgi_application()
