7c001409d2b9d7842c63baed7ca5d82f78428674
LanLop1 <lanchazolopezs@gmail.com>
2024-08-19 18:33:23 +0200

    Cambios y mejoras en la base de datos, añadido un campo imagen que vincule todas las imágenes y permita recogerlas desde diferentes sitios, ademas de poner límites a los textfields para evitar que llenen la memoria con malas intenciones

--- django-starter/a_home/models.py
+++ django-starter/a_home/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 from django.utils import timezone
+class Image(models.Model):
+    file = models.ImageField(upload_to='images/')
+    description = models.TextField(max_length=1255, null=True, blank=True)
+
+    def __str__(self):
+        return f"Image: {self.file.name}"
+    
 class Article(models.Model):
     title = models.CharField(max_length=200)
-    content = models.TextField()
+    content = models.TextField(max_length=8000)
     resumen = models.TextField(max_length=1255)
-    image = models.ImageField(upload_to='images/', blank=True, null=True)
+    image = models.ForeignKey(Image, on_delete=models.SET_NULL, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
@@ -12,9 +19,9 @@ class Article(models.Model):
 
 
 class Template(models.Model):
-    name = models.TextField(null=False)
-    description = models.TextField(null=False)
-    thumbnailurl = models.TextField(null=True)  # Changed to allow null
+    name = models.CharField(max_length=200, null=False)
+    description = models.TextField(max_length=1255, null=False)
+    thumbnailurl = models.TextField(max_length=1255,null=True)  # Changed to allow null
     price = models.DecimalField(max_digits=10, decimal_places=2, null=False)
     premium = models.BooleanField(null=False)
     createdat = models.DateTimeField(default=timezone.now)
@@ -24,11 +31,11 @@ class Template(models.Model):
         return f"Nombre: {self.name}"
 
 class User(models.Model):
-    name = models.TextField(null=False)
-    email = models.TextField(null=False, unique=True)
-    password = models.TextField(null=False)
-    phone = models.TextField(null=False)
-    address = models.TextField(null=False)
+    name = models.CharField(max_length=200, null=False)
+    email = models.EmailField(null=False, unique=True)
+    password = models.CharField(max_length=200, null=False)
+    phone = models.CharField(max_length=200, null=False)
+    address = models.CharField(max_length=200, null=False)
     createdat = models.DateTimeField(default=timezone.now)
     updatedat = models.DateTimeField(default=timezone.now)
     def __str__(self) -> str:
@@ -37,10 +44,10 @@ class User(models.Model):
 class Store(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
     template = models.ForeignKey(Template, on_delete=models.CASCADE, null=False)
-    name = models.TextField(null=False)
-    description = models.TextField(null=False)
-    logourl = models.TextField(null=True)  # Changed to allow null
-    bannerurl = models.TextField(null=True)  # Changed to allow null
+    name = models.CharField(max_length=200, null=False)
+    description = models.TextField(max_length=1255, null=False)
+    logourl = models.TextField(max_length=1255,null=True)  # Changed to allow null
+    bannerurl = models.TextField(max_length=1255,null=True)  # Changed to allow null
     createdat = models.DateTimeField(default=timezone.now)
     updatedat = models.DateTimeField(default=timezone.now)
     def __str__(self) -> str:
@@ -48,11 +55,11 @@ class Store(models.Model):
 
 class Product(models.Model):
     store = models.ForeignKey(Store, on_delete=models.CASCADE, null=False)
-    name = models.TextField(null=False)
-    description = models.TextField(null=False)
+    name = models.CharField(max_length=200, null=False)
+    description = models.TextField(max_length=1255, null=False)
     price = models.DecimalField(max_digits=10, decimal_places=2, null=False)
     stockquantity = models.IntegerField(null=False)
-    image = models.ImageField(upload_to='images/', blank=True, null=True)
+    image = models.ForeignKey(Image, on_delete=models.SET_NULL, null=True, blank=True, related_name='product_images')
     createdat = models.DateTimeField(default=timezone.now)
     updatedat = models.DateTimeField(default=timezone.now)
     def __str__(self) -> str:
@@ -60,8 +67,8 @@ class Product(models.Model):
 
 class Coupon(models.Model):
     store = models.ForeignKey(Store, on_delete=models.CASCADE, null=False)
-    code = models.TextField(null=False,unique=True )
-    discounttype = models.TextField(null=False)
+    code = models.CharField(max_length=100, null=False)
+    discounttype = models.TextField(max_length=1255, null=False)
     discountvalue = models.DecimalField(max_digits=10, decimal_places=2, null=False)
     expirydate = models.DateField(null=False)
     createdat = models.DateTimeField(default=timezone.now)
@@ -70,9 +77,9 @@ class Coupon(models.Model):
         return f"Code of discount, store and discount value: {self.code, self.store, self.discountvalue}"
 
 class SubscriptionPlan(models.Model):
-    planname = models.TextField(null=False, unique=True)
+    planname = models.CharField(max_length=200, null=False, unique=True)
     storagelimit = models.IntegerField(null=False)
-    features = models.TextField(null=False)
+    features = models.TextField(max_length=1255, null=False)
     price = models.DecimalField(max_digits=10, decimal_places=2, null=False)
     createdat = models.DateTimeField(default=timezone.now)
     updatedat = models.DateTimeField(default=timezone.now)
@@ -89,7 +96,7 @@ class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
     store = models.ForeignKey(Store, on_delete=models.CASCADE, null=False)
     totalamount = models.DecimalField(max_digits=10, decimal_places=2, null=False)
-    orderstatus = models.TextField(null=False)
+    orderstatus = models.TextField(max_length=1255,null=False)
     createdat = models.DateTimeField(default=timezone.now)
     updatedat = models.DateTimeField(default=timezone.now)
 
@@ -103,20 +110,20 @@ class OrderItem(models.Model):
 
 class Notification(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
-    message = models.TextField(null=False)
-    readstatus = models.TextField(null=False)
+    message = models.TextField(max_length=8000, null=False)
+    readstatus = models.BinaryField(null=False)
     createdat = models.DateTimeField(default=timezone.now)
     updatedat = models.DateTimeField(default=timezone.now)
 
 class ChatMessage(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages', null=False)
     receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages', null=False)
-    messagecontent = models.TextField(null=False)
+    messagecontent = models.TextField(max_length=8000, null=False)
     sentat = models.DateTimeField(default=timezone.now)
 
 class QRCode(models.Model):
     store = models.ForeignKey(Store, on_delete=models.CASCADE, null=False)
-    image = models.ImageField(upload_to='images/', blank=True, null=True)
+    image = models.ForeignKey(Image, on_delete=models.SET_NULL, null=True, blank=True, related_name='qr_codes')
     createdat = models.DateTimeField(default=timezone.now)
 
 class Invoice(models.Model):
@@ -124,6 +131,7 @@ class Invoice(models.Model):
     issuedate = models.DateField(null=False)
     duedate = models.DateField(null=False)
     totalamount = models.DecimalField(max_digits=10, decimal_places=2, null=False)
-    status = models.TextField(null=False)
+    status = models.TextField(max_length=1255, null=False)
     createdat = models.DateTimeField(default=timezone.now)
     updatedat = models.DateTimeField(default=timezone.now)
+
