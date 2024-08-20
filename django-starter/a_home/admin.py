35794ca8b803565f2f2dfdc7270450dd108afe2f
alma8026 <alikswtf@gmail.com>
2024-08-20 17:02:57 +0200

    django base

--- django-starter/a_home/admin.py
+++ django-starter/a_home/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Template, User, Store, Product, Coupon
 
 admin.site.register(Article)
-
-# Register your models here.
+admin.site.register(Template)
+admin.site.register(User)
+admin.site.register(Store)
+admin.site.register(Product)
+admin.site.register(Coupon)
