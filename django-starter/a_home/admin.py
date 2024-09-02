3c8f63571303938e1a3b6e8d51f25745c635a7c8
alma8026 <alikswtf@gmail.com>
2024-09-02 11:41:07 +0200

    chat, notifications and products, search-bar together

--- django-starter/a_home/admin.py
+++ django-starter/a_home/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Article, Template
+from .models import Article, Template, Image
 
 
 admin.site.register(Article)
 admin.site.register(Template)
-
+admin.site.register(Image)
 
 
--- django-starter/a_users/admin.py
+++ django-starter/a_users/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, SubscriptionPlan, UserSubscription
 
 admin.site.register(Profile)
+admin.site.register(SubscriptionPlan)
+admin.site.register(UserSubscription)
