fcade9c2811baacc2c62dece9c4c2ae204de08b3
LanLop1 <lanchazolopezs@gmail.com>
2024-09-08 00:04:51 +0200

    cambios formularios para que al crear un producto se adjudique automáticamente a la tienda del usuario.

--- django-starter/a_core/urls.py
+++ django-starter/a_core/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 from a_users.views import profile_view
 from a_home.views import *
 from a_home.views import article_detail_view, search_object, product_detail, search_view, empty_view
-from products.views import  add_to_cart, product_detail, product_shop_view
+from products.views import  product_detail, product_shop_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -35,7 +35,6 @@ urlpatterns = [
     path('orders/', include('orders.urls'), name='orders'),
     path('', include('products.urls'), name='productSergio/django-starters'),
     path('stores/', include('stores.urls', namespace='stores')),
-    path('add-to-cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
     path('product/<int:product_id>/modal/', product_detail, name='product_detail'),
     path('product/<int:id>/', product_shop_view, name='product_shop_view'),
 ]
--- django-starter/products/views.py
+++ django-starter/products/views.py
@@ -12,37 +12,46 @@ from django.contrib.auth.decorators import login_required
 @login_required
 @require_http_methods(["GET", "POST"])
 def create_product(request):
-    product_form = ProductForm(user=request.user)
-    image_form = ImageForm()
-
     if request.method == 'POST':
-        if 'product_submit' in request.POST:
-            product_form = ProductForm(request.POST, request.FILES, user=request.user)
-            if product_form.is_valid():
-                product = product_form.save()
-                return HttpResponse(f"<div id='product-{product.id}'>{product.name} creado exitosamente!</div>")
-        elif 'image_submit' in request.POST:
-            image_form = ImageForm(request.POST, request.FILES)
-            if image_form.is_valid():
-                image = image_form.save(commit=False)
-                image.user = request.user
-                image.save()
-                return HttpResponse(f'<option value="{image.id}">{image.file.name}</option>')
+        product_form = ProductForm(request.POST, user=request.user)
+        if product_form.is_valid():
+            product = product_form.save()
+            products = Product.objects.filter(store__owner=request.user)
+            html = render_to_string('product_list_partial.html', {'products': products})
+            return HttpResponse(html)
+    else:
+        product_form = ProductForm(user=request.user)
+        image_form = ImageForm()
     
+    products = Product.objects.filter(store__owner=request.user)
     return render(request, 'create_product.html', {
         'product_form': product_form,
-        'image_form': image_form
+        'image_form': image_form,
+        'products': products
     })
+
+@login_required
+def upload_image(request):
+    if request.method == 'POST':
+        image_form = ImageForm(request.POST, request.FILES)
+        if image_form.is_valid():
+            image = image_form.save(commit=False)
+            image.user = request.user
+            image.save()
+            return HttpResponse(f'<option value="{image.id}">{image.file.name}</option>')
+    return HttpResponse("Error al subir la imagen", status=400)
+
+@login_required
+def product_list(request):
+    products = Product.objects.filter(store__owner=request.user)
+    return render(request, 'product_list_partial.html', {'products': products})
 
 def product_detail(request, product_id):
     product = get_object_or_404(Product, id=product_id)
     html = render_to_string('product_detail.html', {'product': product})
     return HttpResponse(html)
 
-def add_to_cart(request, product_id):
-    # Aquí iría la lógica para añadir el producto al carrito
-    # Por ahora, solo devolvemos un contador ficticio
-    return HttpResponse("1")  # Devuelve el nuevo número de items en el carrito
+
 
 def product_shop_view(request, id):
     product = get_object_or_404(Product, id=id)
