6ee430eb52cc5b8854c4fea5aedb757a1d51b555
alma8026 <alikswtf@gmail.com>
2024-09-13 19:16:26 +0200

    arreglos

--- django-starter/a_home/urls.py
+++ django-starter/a_home/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', home_view, name="pagina_home"),
-    path('search/', search_view, name='search'),
-    path('article/<int:id>/', article_detail_view, name='article_detail'),
+    path('', welcome_view, name="pagina_home"),
 ]
--- django-starter/a_home/views.py
+++ django-starter/a_home/views.py
@@ -3,7 +3,7 @@ from .models import Article
 # def home_view(request):
 #     return render(request, 'home.html')
 
-def home_view(request):
+def welcome_view(request):
      return render(request, 'Pagina_Inicio/index.html')
 
 def search_view(request):
